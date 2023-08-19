22a96a4554f0412b41d177d505a06a99fc52edd0
484yang <141470008+484yang@users.noreply.github.com>
2023-08-19 14:28:27 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def handle_postback(event):
 
     if data.get('action') == 'service':
         service_event(event)
-    elif data.get('action') == 'select_data':
+    elif data.get('action') == 'select_date':
         service_select_date_event(event)
     elif data.get('action') == 'select_time':
         service_select_date_event(event)
